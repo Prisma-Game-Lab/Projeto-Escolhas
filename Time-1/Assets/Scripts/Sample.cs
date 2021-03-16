54e1a81b5863ca7778525b15a82a8cb151da69ea
tati99 <tatireimer99@gmail.com>
2021-03-15 23:33:57 -0300

    Configuração mobile

---
+++ Time-1/Assets/Scripts/Sample.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sample : MonoBehaviour
+{
+    public Text text;
+
+    public void SampleText() {
+ 
+        text.text = "Parabéns, você rodou o unity no celular";
+    }
+}

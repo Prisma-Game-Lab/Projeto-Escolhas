40ff86bdc2c0adbf8aab2638f30ba884ff13df9a
tati99 <tatireimer99@gmail.com>
2021-03-21 12:58:58 -0300

    Fix navigation

---
+++ Time-1/Assets/Scripts/NavigationBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NavigationBar : MonoBehaviour
+{
+    public Canvas home;
+
+    private Canvas _name;
+
+    void Start() {
+        _name = home;
+    }
+    
+    public void SetActiveCanvas(Canvas canvas) {
+        _name.gameObject.SetActive(false);
+        canvas.gameObject.SetActive(true);
+        _name = canvas;
+        
+    }
+
+    public void GoToMenu() {
+        SceneManager.LoadScene("MainMenu_Scene");
+    }
+
+}

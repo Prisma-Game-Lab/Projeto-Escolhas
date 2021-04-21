e1828ee119d7b9b6ebe2862b0b88abbb067d7bac
tati99 <tatireimer99@gmail.com>
2021-04-21 20:29:31 -0300

    Mudanças nos game objects

--- Time-1/Assets/Game/Scripts/InkExample.cs
+++ Time-1/Assets/Game/Scripts/InkExample.cs
@@ -8,104 +8,68 @@ using TMPro;
 public class InkExample : MonoBehaviour
 {
     public TextAsset inkJSONAsset;
-
     private Story story;
-
     public Button buttonPrefab;
     public GameObject textPrefab;
     public GameObject content;
-
+    public GameObject scrollView;
     public GameObject buttonPlace;
-
+    public GameObject firstText;
+    public GameObject topBar;
+    private float posxc;
+    private float posyc;
+    private int lastLine;
+    private float distance;
     private List<string> messages = new List<string>();
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Load the next story block
         story = new Story(inkJSONAsset.text);
 
-        // Start the refresh cycle
-        refresh();
+        lastLine = 0;
+        distance = 0;
 
+        float posxb = buttonPlace.transform.position.x;
+        float posyb = buttonPlace.transform.position.y;
+        float posyt = topBar.transform.position.y;
+
+        scrollView.transform.position = new Vector2(posxb, posyb * 3.8f);
+
+        posxc = firstText.transform.position.x;
+        posyc = firstText.transform.position.y;
+
+        refresh();
     }
 
-    // Refresh the UI elements
-    //  – Clear any current elements
-    //  – Show any text chunks
-    //  – Iterate through any choices and create listeners on them
     void refresh()
     {
-        // Clear the UI
         clearUI();
-
-        // Create a new GameObject
-        //GameObject newGameObject = new GameObject("TextChunk");
-        // Set its transform to the Canvas (this)
-        //newGameObject.transform.SetParent(this.transform, false);
-
-        // Add a new Text component to the new GameObject
-        //Text newTextObject = newGameObject.AddComponent<Text>();
-        // Set the fontSize larger
-        //newTextObject.fontSize = 55;
-
-        // Load the next block and save text (if any)
         getNextStoryBlock();
 
-        List<GameObject> newGameObject1 = new List<GameObject>();
-
-        int i = 0;
-
-        foreach (var line in messages) {
-            Debug.Log(line);
+        for (int i = lastLine; i < messages.Count; i++) {
+            Debug.Log(messages[i]);
             Instantiate(textPrefab, content.transform);
-            textPrefab.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = line;
-            textPrefab.transform.SetParent(content.transform);
-            //GameObject newGameObject2 = new GameObject("line");
-            //newGameObject1.Add(newGameObject2);
-            //newGameObject1[i].AddComponent<Text>();
-            //newGameObject1[i].transform.SetParent(content.transform);
-            //newGameObject1[i].GetComponent<Text>().fontSize = 55;
-            //newGameObject1[i].GetComponent<Text>().text = line;
-            //newGameObject1[i].GetComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-            //i++;
-            //newGameObject.AddComponent<Image>();
-            //if (line.Contains("Player")) {
-                //newGameObject1[i].GetComponent<Text>().text.Alignment = "right";
-            //}
-        }
-
-        foreach (var j in newGameObject1) {
-            Debug.Log(j.GetComponent<Text>().text);
+            textPrefab.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = messages[i];
+            textPrefab.transform.position = new Vector2(posxc, posyc + distance);      
+            posxc = textPrefab.transform.position.x;
+            posyc = textPrefab.transform.position.y;
+            Debug.Log(posyc);
+            posyc += posyc/2;
+            distance = 2.0f;
+            lastLine += 1;
         }
 
         // Get the current tags (if any)
-        List<string> tags = story.currentTags;
-
-        // If there are tags, use the first one.
-        // Otherwise, just show the text.
-        //if (tags.Count > 0)
-        //{
-            //newTextObject.text = "<color=grey>" + tags[0] + "</color> – " + text;
-        //}
-        //else
-        //{
-            //newTextObject.text = text;
-        //}
-
-        // Load Arial from the built-in resources
-        //newTextObject.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        //List<string> tags = story.currentTags;
 
         foreach (Choice choice in story.currentChoices)
         {
             Button choiceButton = Instantiate(buttonPrefab, buttonPlace.transform);
             choiceButton.transform.SetParent(buttonPlace.transform);
 
-            // Gets the text from the button prefab
             Text choiceText = choiceButton.GetComponentInChildren<Text>();
             choiceText.text = choice.text;
 
-            // Set listener
             choiceButton.onClick.AddListener(delegate {
                 OnClickChoiceButton(choice);
             });
@@ -113,47 +77,26 @@ public class InkExample : MonoBehaviour
         }
     }
 
-    // When we click the choice button, tell the story to choose that choice!
     void OnClickChoiceButton(Choice choice)
     {
         story.ChooseChoiceIndex(choice.index);
         refresh();
     }
 
-    // Clear out all of the UI, calling Destory() in reverse
     void clearUI()
     {
         int childCount = buttonPlace.transform.childCount;
         for (int i = childCount - 1; i >= 0; i--)
         {
-            //if (this.transform.GetChild(i).gameObject.tag != "Scroll") {
-            //if (this.transform.GetChild(i).gameObject.tag == "ButtonSpace") {
-                Destroy(buttonPlace.transform.GetChild(i).gameObject); 
-                //}
-                //else {
-                    //GameObject.Destroy(this.transform.GetChild(i).gameObject);
-                //}
-           //}
+            Destroy(buttonPlace.transform.GetChild(i).gameObject); 
         }
     }
 
-
-    // Load and potentially return the next story block
     void getNextStoryBlock()
     {
-
         while (story.canContinue)
         {
-            //text = story.ContinueMaximally();
             messages.Add(story.Continue());
         }
-
-        //return text;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }

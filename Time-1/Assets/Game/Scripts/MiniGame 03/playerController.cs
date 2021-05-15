db88f0db6cf86104dcf20d139d222cce9b5170f0
vitorsalek <59521603+vitorsalek@users.noreply.github.com>
2021-05-15 12:07:10 -0300

    Minigame 3 meio pronto

--- Time-1/Assets/Game/Scripts/MiniGame 03/playerController.cs
+++ Time-1/Assets/Game/Scripts/MiniGame 03/playerController.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public float rotationSpeed;
     float rotationX=0;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    private Touch touch;
+    private Quaternion Xrotation;
 
-    // Update is called once per frame
     void Update()
     {
-        // if (Input.GetMouseButton(0))
-        // {
-        float h = 2f * Input.GetAxis("Mouse X");
-        Debug.Log(h);
-        transform.Rotate(0, 0, h);
-            //rotationX += Input.GetAxis("Mouse X") * 10f;
-            //rotationX = normalizeAngle(rotationX);
-           //print(Input.GetAxis("Mouse X") * 10f);
-            //Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.forward);
-            //transform.localRotation = xQuaternion;
-            
-       // }
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Xrotation = Quaternion.Euler(0f, 0f, -touch.deltaPosition.x * rotationSpeed);
+                transform.rotation = Xrotation * transform.rotation;
+                //rotationX += -touch.deltaPosition.x*rotationSpeed*Time.deltaTime;
+                //rotationX = normalizeAngle(rotationX);
+                //Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.forward);
+                //transform.localRotation = xQuaternion;
+            }
+        }
+
+        //FUNCIONA PARA WEB
+        //if (Input.GetMouseButton(0))
+        //{
+        //    rotationX += Input.GetAxis("Mouse X") * rotationSpeed;
+        //    rotationX = normalizeAngle(rotationX);
+        //    Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.forward);
+        //    transform.localRotation = xQuaternion;
+        //}
+
     }
 
     private float normalizeAngle(float angle)

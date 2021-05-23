861dfbb5bac80327a43cdd88651dc2b2d3464a52
tati99 <tatireimer99@gmail.com>
2021-05-23 01:22:19 -0300

    Feedback no back button

--- Time-1/Assets/Game/Scripts/SaveSystem/AppSave.cs
+++ Time-1/Assets/Game/Scripts/SaveSystem/AppSave.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Save", menuName = "Save/Scriptable Obj")]
 public class AppSave : ScriptableObject
 {
     public List<GameObject> elfa;

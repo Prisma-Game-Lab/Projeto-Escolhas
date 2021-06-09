e4c1187c1aad7c0f0f7251613ce9fafba88e1bb8
tati99 <tatireimer99@gmail.com>
2021-06-09 12:33:43 -0300

    Trocando UI por sprite renderer

--- Time-1/Assets/Game/Scripts/Minigame 03/HammerGame.cs
+++ Time-1/Assets/Game/Scripts/Minigame 03/HammerGame.cs
@@ -6,22 +6,17 @@ public class HammerGame : MonoBehaviour
 {
     public GameObject indicator;
     public GameObject bar;
-    private float barx, bary, height;
-    private float speed = 10.5f;
-    private float newPos;
-
+    public float speed;
+    
     void Start()
     {
-        bary = bar.transform.position.y;
-        Debug.Log(bar.GetComponent<RectTransform>().rect.height);
     }
 
    
     void Update()
     {
         float posy = indicator.transform.position.y;
-        if ((posy <= bary - bar.GetComponent<RectTransform>().rect.height*0.27f ) || (posy >= bary + bar.GetComponent<RectTransform>().rect.height*0.27f )) {
-            Debug.Log(posy);
+        if ((posy <= -2.16f) || (posy >= 4.63f)) {
             speed *= -1;
         }
         indicator.transform.position = new Vector2(indicator.transform.position.x, posy - speed);
